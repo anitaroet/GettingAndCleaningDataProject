8edfc0c74661b310d3355f50e339755728ff33dd
Anita Røtvold <anita.rotvold@gmail.com>
2021-10-25 19:16:24 +0200

    Created tidy dataset with average for each mean and stdev for each activity and subject

--- run_analysis.R
+++ run_analysis.R
@@ -1,38 +1,47 @@
 #Loading libraries and functions
 library(tidyverse)
 
+
+# 1) Load and merge data
 source("read_textfile.R")
-
-#Reading data
-train <- read_textfile(y="train/X_train.txt")
-train_labels <- read_textfile(y="train/y_train.txt")
-subject_train <- read_textfile(y="train/subject_train.txt")
-
-test <- read_textfile(y="test/X_test.txt")
-test_labels <- read_textfile(y="test/y_test.txt")
-subject_test <- read_textfile(y="test/subject_test.txt")
-
-activity_labels <- read_textfile(y="activity_labels.txt")
-features <- read_textfile(y="features.txt")
+source("load_merge.R")
 
 
-#Changing columnnames of training- and test data
-colnames(train)=features$V2
-colnames(test)=features$V2
+# 2) Extract mean and standard deviations
 
-colnames(subject_train)="subject"
-colnames(subject_test)="subject"
+  # Find which columns to keep
+    features_use <- features %>%
+      mutate(use=grepl(("mean|std"),V2)) %>%
+      filter(use==TRUE) %>%
+      mutate(V1=V1+2) %>%
+      select(V1)
 
-colnames(train_labels)="activity"
-colnames(test_labels)="activity"
+  # Filter columns
+    dat_filtered <- dat[,c(1,2,features_use$V1)]  
+    
 
-#Combining training-and testdata with subjects
-train_data <- cbind(subject_train, train_labels, train)
-test_data <- cbind(subject_test, test_labels, test)
+# 3) Name activities
+    
+    dat_named <- dat_filtered %>%
+      mutate(activity=ifelse(activity==1, "Walking", ifelse(
+        activity==2, "Walking upstairs", ifelse(
+          activity==3, "Walking downstairs", ifelse(
+            activity==4, "Sitting", ifelse(
+              activity==5, "Standing", "Laying"
+            )
+          )
+        )
+      )))
+      
+      
+# 4) Descriptive variable names
+    
+    dat_long <- dat_named %>%
+      gather(key="variable", value="value",3:81 )
+    
 
-#Identify each dataset
-train_data$dataset="train"
-test_data$dataset="test"
-
-#Merges into one dataset
-dat <- rbind(train_data, test_data)
+# 5) Make new dataset with means
+    
+    dat_average <- dat_long %>%
+      group_by(variable, activity, subject) %>%
+      summarize(average=mean(value))
